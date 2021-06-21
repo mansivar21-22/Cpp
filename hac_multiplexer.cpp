a69ccad0b4333f5270fcff7874da1b8b29c0657e
Mansi Varshney <70686638+mansivar21-22@users.noreply.github.com>
2021-06-21 23:37:22 +0530

    Add files via upload

---
+++ hac_multiplexer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int base , exp , result,num ;
+    //cin >> base >> exp;
+  //  pow(base, exp);
+   // result = pow(base,exp);
+    cin >> result;
+    num = sqrt(result); 
+    cin >> num;
+    if (result == pow(base,exp))
+    
+        cout << exp;/* code */
+    else
+    
+        cout << "Not possible";
+    return 0;
+}
